612243a0e653efb153b5dc674fbf9918807bb6f2
Nap fvn <phugogang@hotmail.com>
2017-05-21 12:28:35 +0700

    better

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.enable('trust proxy');
 
+app.get('/', (req, res) => {
+    res.redirect('/api/whoami');
+})
+
 app.get('/api/whoami', (req, res) => {
     var ip = (req.headers['x-forwarded-for'] ||
      req.connection.remoteAddress ||
