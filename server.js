6703e7b1d3aa7a89129286d3e534b0c97f6f67ba
Nap fvn <phugogang@hotmail.com>
2017-05-21 12:26:33 +0700

    make it better

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+app.enable('trust proxy');
+
+app.get('/api/whoami', (req, res) => {
+    var ip = (req.headers['x-forwarded-for'] ||
+     req.connection.remoteAddress ||
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress)
+
+     var header = {
+         ipaddress: ip,
+         language: req.headers['accept-language'].split(',')[0],
+         software: req.headers['user-agent'].split('(')[1].split(')')[0]
+     }
+
+
+    res.json(header);
+});
+
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log('Listenning 3000 port');
+})
